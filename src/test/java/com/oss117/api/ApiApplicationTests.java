c5f3c2961a7b074efcbea05194d42b88486af7bb
kenny <kenny.caldieraro@gmail.com>
2023-05-13 12:56:44 +0200

    first commit

---
+++ src/test/java/com/oss117/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oss117.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
